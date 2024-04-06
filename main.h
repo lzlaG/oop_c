c1363b6d877dbfabcb3450f41f80030e672cc543
lzlaG <p0chta1234prosto@yandex.ru>
2024-04-07 03:35:41 +0500

    РАБОТАЕТ

--- main.h
+++ main.h
@@ -38,6 +38,7 @@ class Scum
             AgeOfMutant = GetRandomAge();
         };
     public:
+
         StregthOfHands GetHandPower() const{return HandPower;};
         StregthOfLegs GetLegPower() const{return LegPower;};
         Age GetAgeOfMutant() const{return AgeOfMutant;};
@@ -47,5 +48,74 @@ class Scum
 };
 
 typedef Scum * ScumPointer;
+
+
+class MutantContainerIterator : public Iterator<ScumPointer>
+{
+    private:
+        const ScumPointer *ScumCell;
+        int Pos;
+        int Count;
+    public:
+        MutantContainerIterator(const ScumPointer *scumcell, int count)
+        {
+            ScumCell = scumcell;
+            Count = count;
+            Pos = 0;
+        };
+        void First() { Pos = 0; }
+        void Next() { Pos++; }
+        bool IsDone() const { return Pos >= Count;}
+        ScumPointer GetCurrent() const { return ScumCell[Pos];}
+};
+
+class MutantContainer
+{
+    private:
+        ScumPointer * ScumCell;
+        int MutantCount;
+        int MaxSize;
+    public:
+        MutantContainer(int MaxSize);
+        virtual ~MutantContainer();
+        void AddMutant(ScumPointer newMutant);
+        int GetCount() const {return MutantCount; };
+        ScumPointer GetByIndex(int index) const {return ScumCell[index];}
+        Iterator<ScumPointer> * GetIterator()
+        {
+            return new MutantContainerIterator(ScumCell, MutantCount);
+        };
+};
+
+void MutantContainer::AddMutant(ScumPointer newMutant)
+{
+    ScumCell[MutantCount] = newMutant;
+    MutantCount++;
+}
+MutantContainer::MutantContainer(int maxSize)
+{
+    ScumCell = new ScumPointer[maxSize];
+    for (int i = 0; i<maxSize; i++)
+    {
+        ScumCell[i] = NULL;
+    };
+    MutantCount = 0;
+    MaxSize = maxSize;
+};
+
+MutantContainer::~MutantContainer()
+{
+    for (int i = 0; i<MaxSize; i++)
+    {
+        if( ScumCell[i] != NULL)
+        {
+            delete ScumCell[i];
+            ScumCell[i] = NULL;
+        };
+    };
+    delete[] ScumCell;
+};
+
+
 
 #endif ScumH
--- test.cpp
+++ test.cpp
@@ -5,30 +5,102 @@
 
 using namespace std;
 
+StregthOfHands GetRandomHandPower()
+{
+    int x = random()%(3-1+1)+1;
+    switch (x)
+    {
+        case 1:
+            return StregthOfHands::High;
+        case 2:
+            return StregthOfHands::Medium;
+        case 3:
+            return StregthOfHands::Low;
+    };
+};
+
+StregthOfLegs GetRandomLegPower()
+{
+   int x = random()%(3-1+1)+1;
+    switch (x)
+    {
+        case 1:
+            return StregthOfLegs::High;
+        case 2:
+            return StregthOfLegs::Medium;
+        case 3:
+            return StregthOfLegs::Low;
+    }; 
+};
+
+Age GetRandomAge()
+{
+    int x = random()%(3-1+1)+1;
+    switch (x)
+    {
+        case 1:
+            return Age::Young;
+        case 2:
+            return Age::Old;
+        case 3:
+            return Age::Newborn;
+    }; 
+};
+
 class Gargoyle : public Scum
 {
     public:
-        MutantType GetType() const {return MutantType::Gargoyle;}
-        void Summon() { cout << " Призвали горгулью " << "\n";}
-        void Kill() { cout << " убили горгулью " << "\n";}
+        MutantType GetType() const {return MutantType::Gargoyle;};
+        void Summon()   const { cout << " Призвали горгулью " << endl;};
+        void Kill() const { cout << " убили горгулью " << endl;};
 };
 
 class Wolfman : public Scum
 {
     public:
-        MutantType GetType() const {return MutantType::Wolfman;}
-        void Summon() { cout << " Призвали оборотня " << "\n";}
-        void Kill() { cout << " убили оборотня " << "\n";}
+        MutantType GetType() const {return MutantType::Wolfman;};
+        void Summon() const { cout << " Призвали оборотня " << "\n";};
+        void Kill() const { cout << " убили оборотня " << "\n";};
 };
 
 class Vampire : public Scum
 {
     public:
         MutantType GetType() const {return MutantType::Vampire;}
-        void Summon() { cout << " Призвали вампира " << "\n";}
-        void Kill() { cout << " убили вампира" << "\n";}
+        void Summon() const { cout << " Призвали вампира " << "\n";}
+        void Kill() const { cout << " убили вампира" << "\n";}
 };
+
+string PrintMutantType(const MutantType type)
+{
+    switch(type)
+	{
+		case MutantType::Gargoyle: return  "ГОРГУЛЬЯ";
+		case MutantType::Vampire: return  "ВАМПИР";
+		case MutantType::Wolfman: return  "ОБОРОТЕНЬ";
+		default: return "неизвестный";
+	}
+};
+
+void Task1(MutantContainer *scumcell)
+{
+    for (int i=0; i<scumcell->GetCount(); i++)
+    {
+        const ScumPointer currentMutant = scumcell->GetByIndex(i);
+        if (currentMutant->GetType() == MutantType::Wolfman)
+        {
+            cout << i << "   " << PrintMutantType(currentMutant->GetType())<< "\n"; 
+        };
+    };
+};
+
 int main()
 {
-    cout << "hello world" << "\n";
+    //cout << "hello world" << "\n";
+    MutantContainer scumcell(100);
+    for (int i = 0; i<50; i++)
+    {
+        scumcell.AddMutant(new Wolfman);
+    };
+    Task1(&scumcell);
 };
